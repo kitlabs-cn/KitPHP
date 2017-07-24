1219799ec5bc0fa61cf96ddf8beaef26fd889e8b
李春鹏 <lcp0578@gmail.com>
2017-07-24 23:29:50 +0800

    [MOD] update views

--- src/KitAdminBundle/Menu/AdminBuilder.php
+++ src/KitAdminBundle/Menu/AdminBuilder.php
@@ -38,10 +38,6 @@ class AdminBuilder implements ContainerAwareInterface
             'route' => 'kit_admin_homepage',
             'label' => '欢迎页'
         ]);
-        $menu['kit_admin_homepage']->addChild('kit_admin_flow', [
-            'route' => 'kit_admin_flow',
-            'label' => '执法流程图'
-        ]);
         $menu['kit_admin_homepage']->addChild('kit_admin_user', [
             'route' => 'kit_admin_user',
             'label' => '前台用户'
@@ -64,28 +60,6 @@ class AdminBuilder implements ContainerAwareInterface
         $menu['kit_admin_homepage']->addChild('kit_admin_user_check', [
             'route' => 'kit_admin_user_check',
             'label' => '用户审核',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_admin_homepage']->addChild('kit_admin_appeal', [
-            'route' => 'kit_admin_appeal',
-            'label' => '申诉陈辩'
-        ]);
-        $menu['kit_admin_homepage']->addChild('kit_admin_appeal_show', [
-            'route' => 'kit_admin_appeal_show',
-            'label' => '申诉详情',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_admin_homepage']->addChild('kit_admin_report', [
-            'route' => 'kit_admin_report',
-            'label' => '群众举报'
-        ]);
-        $menu['kit_admin_homepage']->addChild('kit_admin_report_show', [
-            'route' => 'kit_admin_report_show',
-            'label' => '举报详情',
             'attributes' => [
                 'show' => 'off'
             ]
@@ -155,137 +129,6 @@ class AdminBuilder implements ContainerAwareInterface
 //             'routeParameters' => [
 //                 'id' => $this->getRequest()->get('id')
 //             ]
-        ]);
-        
-        $menu->addChild('kit_video_list', array(
-            'route' => 'kit_video_list',
-            'label' => '视频',
-            'attributes' => [
-                'icon' => 'icon-video-camera'
-            ]
-        ));
-        $menu['kit_video_list']->addChild('kit_video_list', [
-            'route' => 'kit_video_list',
-            'label' => '视频列表'
-        ]);
-        $menu['kit_video_list']->addChild('kit_video_add', [
-            'route' => 'kit_video_add',
-            'label' => '上传视频'
-        ]);
-        
-        $menu['kit_video_list']->addChild('kit_video_log', [
-            'route' => 'kit_video_log',
-            'label' => '视频日志'
-        ]);
-        $menu['kit_video_list']->addChild('kit_video_edit', [
-            'route' => 'kit_video_edit',
-            'label' => '日志编辑',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_video_list']->addChild('kit_video_log_show', [
-            'route' => 'kit_video_log_show',
-            'label' => '日志查看',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_video_list']->addChild('kit_video_show', [
-            'route' => 'kit_video_show',
-            'label' => '视频查看',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        
-        
-        $menu->addChild('kit_scene_homepage', array(
-            'route' => 'kit_scene_homepage',
-            'label' => '案件',
-            'attributes' => [
-                'icon' => 'icon-anchor'
-            ]
-        ));
-        $menu['kit_scene_homepage']->addChild('kit_scene_homepage', [
-            'route' => 'kit_scene_homepage',
-            'label' => '案件管理'
-        ]);
-        $menu['kit_scene_homepage']->addChild('matter_index', [
-            'route' => 'matter_index',
-            'label' => '案件列表'
-        ]);
-        $menu['kit_scene_homepage']->addChild('matter_new', [
-            'route' => 'matter_new',
-            'label' => '立案呈报'
-        ]);
-        $menu['kit_scene_homepage']->addChild('matter_show', [
-            'route' => 'matter_show',
-            'label' => '案件详情',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_scene_homepage']->addChild('matter_examine', [
-            'route' => 'matter_examine',
-            'label' => '案件审批',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_scene_homepage']->addChild('matter_edit', [
-            'route' => 'matter_edit',
-            'label' => '案件编辑',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_scene_homepage']->addChild('report_index', [
-            'route' => 'report_index',
-            'label' => '调查报告列表'
-        ]);
-        $menu['kit_scene_homepage']->addChild('report_new', [
-            'route' => 'report_new',
-            'label' => '新增调查报告'
-        ]);
-        $menu['kit_scene_homepage']->addChild('report_show', [
-            'route' => 'report_show',
-            'label' => '调查报告详情',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_scene_homepage']->addChild('report_examine', [
-            'route' => 'report_examine',
-            'label' => '调查报告审核',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_scene_homepage']->addChild('notice_index', [
-            'route' => 'notice_index',
-            'label' => '通知公告'
-        ]);
-        $menu['kit_scene_homepage']->addChild('notice_new', [
-            'route' => 'notice_new',
-            'label' => '新增通知公告',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_scene_homepage']->addChild('notice_show', [
-            'route' => 'notice_show',
-            'label' => '查看通知公告',
-            'attributes' => [
-                'show' => 'off'
-            ]
-        ]);
-        $menu['kit_scene_homepage']->addChild('notice_edit', [
-            'route' => 'notice_edit',
-            'label' => '编辑通知公告',
-            'attributes' => [
-                'show' => 'off'
-            ]
         ]);
         return $menu;
     }
