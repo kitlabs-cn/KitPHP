1e42882a87f144690ff359cba775d966c4a66513
李春鹏 <lcp0578@gmail.com>
2017-09-27 23:28:24 +0800

    [MOD] update file uploader service

--- src/KitBaseBundle/Service/FileUploader.php
+++ src/KitBaseBundle/Service/FileUploader.php
@@ -2,22 +2,52 @@
 namespace KitBaseBundle\Service;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\DependencyInjection\Container;
 
 class FileUploader
 {
     private $targetDir;
+    private $container;
 
-    public function __construct($targetDir)
+    public function __construct($targetDir,Container $container)
     {
         $this->targetDir = $targetDir;
+        $this->container = $container;
     }
 
     public function upload(UploadedFile $file, $subDir = '')
     {
         $fileName = md5(uniqid()).'.'.$file->guessExtension();
-        $subDir = empty($subDir) ? '' : DIRECTORY_SEPARATOR . $subDir;
-        $file->move($this->targetDir . $subDir, $fileName);
+        $subDir = empty($subDir) ? '' : '/'. $subDir;
+        try {
+            $file->move($this->targetDir . $subDir, $fileName);
+            $file->getPath();
+        }catch (\Exception $e){
+            return [
+                'code' => 0,
+                'data' => $e->getCode() . $e->getMessage()
+            ];
+        }
 
-        return $subDir . DIRECTORY_SEPARATOR .$fileName;
+        return [
+            'code' => 1,
+            'data' => 'uploads' .$subDir . '/' .$fileName
+        ];
+    }
+    
+    public function uploadOss(UploadedFile $file, $bucket)
+    {
+        $filename = $this->generateName($file);
+        /**
+         *
+         * @var \Kit\Bundle\OssBundle\Service\ImageService $imageService
+         */
+        $imageService = $this->container->get('kit_oss.image_service');
+        return $imageService->upload($bucket, $filename, $file->getPathname());
+    }
+    
+    private function generateName($file)
+    {
+        return 'images/' .time().'_'.uniqid().'.'.$file->guessExtension();
     }
 }
